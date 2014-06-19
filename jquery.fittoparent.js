acf9e632ef4fc7605808897361e633866afc03ba
Gianluca Guarini <gianluca.guarini@gmail.com>
2014-06-19 11:47:49 +0200

    Update jquery.fittoparent.js

--- jquery.fittoparent.js
+++ jquery.fittoparent.js
@@ -17,135 +17,136 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-(function($) {
-	$.fn.fitToParent = function(options) {
-		var o = $.extend({
-			fitX: true,
-			fitY: true,
-			fillArea: true,
-			allowEnlargement: true,
-			parent: null,
-			inlcudepadding: true,
-			scrollWrapper: false
-		}, options);
-		$(this).each(function() {
-			var $this = $(this),
-				imagePosition = {
-					top: "auto",
-					left: "auto",
-					bottom: "auto",
-					right: "auto"
-				},
-				// this value could be assigned to the image trough the data-alignement attribute
-				// by default this value is center-center
-				alignement = $this.data("alignement") || "center-center";
+(function ($) {
+  $.fn.fitToParent = function (options) {
+    var o = $.extend({
+      fitX: true,
+      fitY: true,
+      fillArea: true,
+      allowEnlargement: true,
+      parent: null,
+      scale: 1,
+      inlcudepadding: true,
+      scrollWrapper: false
+    }, options);
+    $(this).each(function () {
+      var $this = $(this),
+        imagePosition = {
+          top: "auto",
+          left: "auto",
+          bottom: "auto",
+          right: "auto"
+        },
+        // this value could be assigned to the image trough the data-alignement attribute
+        // by default this value is center-center
+        alignement = $this.data("alignement") || "center-center";
 
-			var performScale = function() {
-				var parent = o.parent || $this.parent(),
-					parentWidth = o.inlcudepadding ? parent.innerWidth() : parent.width(),
-					parentHeight = o.inlcudepadding ? parent.innerHeight() : parent.height(),
-					originalSize = $this.data('original-size');
-				if (!originalSize) {
-					originalSize = {
-						width: $this.width(),
-						height: $this.height()
-					};
-					$this.data('original-size', originalSize);
-				}
+      var performScale = function () {
+        var parent = o.parent || $this.parent(),
+          parentWidth = (o.inlcudepadding ? parent.innerWidth() : parent.width()) * o.scale,
+          parentHeight = (o.inlcudepadding ? parent.innerHeight() : parent.height()) * o.scale,
+          originalSize = $this.data('original-size');
+        if (!originalSize) {
+          originalSize = {
+            width: $this.width(),
+            height: $this.height()
+          };
+          $this.data('original-size', originalSize);
+        }
 
-				var currentSize = {
-					width: originalSize.width,
-					height: originalSize.height
-				};
+        var currentSize = {
+          width: originalSize.width,
+          height: originalSize.height
+        };
 
-				var scale = 1;
-				if (o.fitX && (o.allowEnlargement || currentSize.width > parentWidth)) {
-					scale = parentWidth / currentSize.width;
-					currentSize.width = Math.floor(currentSize.width * scale) + 1;
-					currentSize.height = Math.floor(currentSize.height * scale) + 1;
-				}
-				if (o.fitY) {
-					var doScale = o.fillArea ? currentSize.height < parentHeight : currentSize.height > parentHeight,
-						newScale = parentHeight / currentSize.height;
+        var scale = 1;
+        if (o.fitX && (o.allowEnlargement || currentSize.width > parentWidth)) {
+          scale = parentWidth / currentSize.width;
+          currentSize.width = Math.floor(currentSize.width * scale) + 1;
+          currentSize.height = Math.floor(currentSize.height * scale) + 1;
+        }
+        if (o.fitY) {
+          var doScale = o.fillArea ? currentSize.height < parentHeight : currentSize.height > parentHeight,
+            newScale = parentHeight / currentSize.height;
 
-					//if (o.allowEnlargement) doScale = newScale > scale;
-					if (doScale) {
-						currentSize.width = Math.floor(currentSize.width * newScale) + 1;
-						currentSize.height = Math.floor(currentSize.height * newScale) + 1;
-						scale = newScale;
-					}
-				}
+          //if (o.allowEnlargement) doScale = newScale > scale;
+          if (doScale) {
+            currentSize.width = Math.floor(currentSize.width * newScale) + 1;
+            currentSize.height = Math.floor(currentSize.height * newScale) + 1;
+            scale = newScale;
+          }
+        }
 
-				// Deciding where to allign the image
-				switch (alignement) {
-					case "left-top":
-						imagePosition.top = 0;
-						imagePosition.left = 0;
-						break;
-					case "center-top":
-						imagePosition.top = 0;
-						imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
-						break;
-					case "right-top":
-						imagePosition.top = 0;
-						imagePosition.right = 0;
-						break;
-					case "left-center":
-						imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
-						imagePosition.left = 0;
-						break;
-					case "center-center":
-						imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
-						imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
-						break;
-					case "right-center":
-						imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
-						imagePosition.right = 0;
-						break;
-					case "left-bottom":
-						imagePosition.bottom = 0;
-						imagePosition.left = 0;
-						break;
-					case "center-bottom":
-						imagePosition.bottom = 0;
-						imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
-						break;
-					case "right-bottom":
-						imagePosition.bottom = 0;
-						imagePosition.right = 0;
-						break;
-					default:
-						imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
-						imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
-				}
-				if (o.scrollWrapper) {
-					$this.css({
-						width: currentSize.width,
-						height: currentSize.height
-					});
-					parent.prop({
-						scrollLeft: -imagePosition.left,
-						scrollTop: -imagePosition.top
-					});
-				} else {
-					$this.css({
-						top: imagePosition.top,
-						left: imagePosition.left,
-						bottom: imagePosition.bottom,
-						right: imagePosition.right,
-						width: currentSize.width,
-						height: currentSize.height,
-						position:'absolute'
-					});
-				}
+        // Deciding where to allign the image
+        switch (alignement) {
+        case "left-top":
+          imagePosition.top = 0;
+          imagePosition.left = 0;
+          break;
+        case "center-top":
+          imagePosition.top = 0;
+          imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
+          break;
+        case "right-top":
+          imagePosition.top = 0;
+          imagePosition.right = 0;
+          break;
+        case "left-center":
+          imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
+          imagePosition.left = 0;
+          break;
+        case "center-center":
+          imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
+          imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
+          break;
+        case "right-center":
+          imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
+          imagePosition.right = 0;
+          break;
+        case "left-bottom":
+          imagePosition.bottom = 0;
+          imagePosition.left = 0;
+          break;
+        case "center-bottom":
+          imagePosition.bottom = 0;
+          imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
+          break;
+        case "right-bottom":
+          imagePosition.bottom = 0;
+          imagePosition.right = 0;
+          break;
+        default:
+          imagePosition.top = Math.round((parentHeight - currentSize.height) / 2);
+          imagePosition.left = Math.round((parentWidth - currentSize.width) / 2);
+        }
+        if (o.scrollWrapper) {
+          $this.css({
+            width: currentSize.width,
+            height: currentSize.height
+          });
+          parent.prop({
+            scrollLeft: -imagePosition.left,
+            scrollTop: -imagePosition.top
+          });
+        } else {
+          $this.css({
+            top: imagePosition.top,
+            left: imagePosition.left,
+            bottom: imagePosition.bottom,
+            right: imagePosition.right,
+            width: currentSize.width,
+            height: currentSize.height,
+            position: 'absolute'
+          });
+        }
 
-			};
+      };
 
-			if ($this[0].tagName.toLowerCase() === 'img' && !$this[0].width) {
-				$this[0].onload = performScale;
-			} else {
-				performScale();
-			}
-		});
-	};
+      if ($this[0].tagName.toLowerCase() === 'img' && !$this[0].width) {
+        $this[0].onload = performScale;
+      } else {
+        performScale();
+      }
+    });
+  };
 })(jQuery);
